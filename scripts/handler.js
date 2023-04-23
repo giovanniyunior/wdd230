82d8b0297134807b2cb40711b4347043f5eceffb
Giovanni Ulloque <55454652+giovanniyunior@users.noreply.github.com>
2023-04-23 02:37:55 +0000

    js

--- scripts/handler.js
+++ scripts/handler.js
@@ -1,6 +1,6 @@
 
 window.onload = function(){
     const date = new Date(document.lastModified);
-    document.getElementById("lastModifiedDate").innerHTML = date;
+    document.getElementById("lastModifiedDate").innerHTML = date.toLocaleString();
     console.log('IN JS');
 }
