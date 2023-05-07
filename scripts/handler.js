cbe67b12b1d335477d6e73f648d1323313f54100
Giovanni Ulloque <55454652+giovanniyunior@users.noreply.github.com>
2023-05-07 04:49:14 +0000

    copyright

--- scripts/handler.js
+++ scripts/handler.js
@@ -4,6 +4,6 @@ window.onload = function(){
     document.getElementById("lastModifiedDate").innerHTML = date.toLocaleString();
     console.log('IN JS');
 
-    document.querySelector("div.copyright").innerHTML = "&copy; "+ date.getFullYear();
+    document.querySelector("p.copyright").innerHTML = "&copy; "+ date.getFullYear();
 
 }
