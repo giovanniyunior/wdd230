e98959dd3b285789d9733b749302428d121119fc
Giovanni Ulloque <55454652+giovanniyunior@users.noreply.github.com>
2023-05-07 04:47:44 +0000

    js

--- scripts/handler.js
+++ scripts/handler.js
@@ -3,4 +3,7 @@ window.onload = function(){
     const date = new Date(document.lastModified);
     document.getElementById("lastModifiedDate").innerHTML = date.toLocaleString();
     console.log('IN JS');
+
+    document.querySelector("div.copybrite").innerHTML = "&copy; "+ date().getFullYear();
+
 }
