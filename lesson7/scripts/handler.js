f5256c1f0e5e3c33e7f2d305095339c309f7ec7f
Giovanni Ulloque <55454652+giovanniyunior@users.noreply.github.com>
2023-06-03 22:55:17 +0000

    css

--- lesson7/scripts/handler.js
+++ lesson7/scripts/handler.js
@@ -7,3 +7,24 @@ window.onload = function(){
     document.querySelector("span.copyright").innerHTML = "&copy; "+ date.getFullYear();
 
 }
+const images = document.querySelectorAll("[data-src]");
+function preloadImage(img){
+    const src = img.getAttribute("data-src");
+    if(!src){
+        return;
+    }
+    img.src = src;
+}
+const imgOptions = {};
+const imgObserver = new IntersectionObserver((entries,imgObserver) => {
+    if(!FileSystemEntry.isIntersecting){
+        return;
+    } else{
+        preloadImage(entry.target);
+        imgObserver.unobserve(entry.target);
+    }
+},imgOptions);
+
+images.forEach(image => {
+    imgObserver.observe(image);
+})
