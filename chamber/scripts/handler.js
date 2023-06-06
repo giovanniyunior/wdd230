a27dfdfcceeb4f3d30615d9a8668b5cb93d5b5f1
Giovanni Ulloque <55454652+giovanniyunior@users.noreply.github.com>
2023-06-06 01:09:02 +0000

    css

---
+++ chamber/scripts/handler.js
@@ -0,0 +1,9 @@
+
+window.onload = function(){
+    const date = new Date(document.lastModified);
+    document.getElementById("lastModifiedDate").innerHTML = date.toLocaleString();
+    console.log('IN JS');
+
+    document.querySelector("span.copyright").innerHTML = "&copy; "+ date.getFullYear();
+
+}
