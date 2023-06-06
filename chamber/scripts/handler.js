8dc21a6b165a5a48f124fffa1e42b9cefe33196c
Giovanni Ulloque <55454652+giovanniyunior@users.noreply.github.com>
2023-06-06 23:32:43 +0000

    css

--- chamber/scripts/handler.js
+++ chamber/scripts/handler.js
@@ -1,17 +1,17 @@
 
-window.onload = function(){
-    const date = new Date(document.lastModified);
-    document.getElementById("lastModifiedDate").innerHTML = date.toLocaleString();
-    console.log('IN JS');
+window.onload = function () {
+  const date = new Date(document.lastModified);
+  document.getElementById("lastModifiedDate").innerHTML = date.toLocaleString();
+  console.log('IN JS');
 
-    document.querySelector("span.copyright").innerHTML = "&copy; "+ date.getFullYear();
+  document.querySelector("span.copyright").innerHTML = "&copy; " + date.getFullYear();
 
-    function navFunction() {
-        var x = document.getElementById("myTopnav");
-        if (x.className === "topnav") {
-          x.className += " responsive";
-        } else {
-          x.className = "topnav";
-        }
-      }
+  function navFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
 }
