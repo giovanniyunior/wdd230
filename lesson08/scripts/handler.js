c6ae9a0fe0e16efc253def061495a8bad5b26ab0
Giovanni Ulloque <55454652+giovanniyunior@users.noreply.github.com>
2023-06-09 01:16:55 +0000

    css

---
+++ lesson08/scripts/handler.js
@@ -0,0 +1,64 @@
+
+window.onload = function(){
+    const date = new Date(document.lastModified);
+    document.getElementById("lastModifiedDate").innerHTML = date.toLocaleString();
+    console.log('IN JS');
+
+    document.querySelector("span.copyright").innerHTML = "&copy; "+ date.getFullYear();
+
+    
+// Check if local storage is supported
+if (typeof Storage !== "undefined") {
+    const lastVisitTimestamp = localStorage.getItem("lastVisitTimestamp");
+  
+    // Get the current date as UTC timestamp
+    const currentTimestamp = Math.floor(Date.now() / 1000);
+  
+    // Calculate the difference in seconds between the current visit and the last visit
+    const timeDifference = currentTimestamp - (lastVisitTimestamp || currentTimestamp);
+  
+    // Calculate the number of days between the visits
+    const daysDifference = Math.round(timeDifference / (60 * 60 * 24));
+  
+    // Save the current visit timestamp in local storage
+    localStorage.setItem("lastVisitTimestamp", currentTimestamp);
+  
+    // Display the number of days since the last visit
+    document.getElementById("daysSinceLastVisit").innerHTML = daysDifference.toString();
+
+  } else {
+    console.error("Local storage is not supported.");
+  }
+}
+const images = document.querySelectorAll("img[data-src]");
+function preloadImage(img){
+    const src = img.getAttribute("data-src");
+    if(!src){
+        return;
+    }
+    img.src = src;
+    img.style.opacity = 1;
+    img.style.transition = "opacity 0.5s";
+
+
+}
+const imgOptions = {
+    threshold:0,
+    rootMargint: "opx 0px 300px 0px"
+};
+const imgObserver = new IntersectionObserver((entries,imgObserver) => {
+    entries.forEach(entry =>{
+        if(!entry.isIntersecting){
+            return;
+        } else{
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+    });
+
+},imgOptions);
+
+images.forEach(image => {
+    image.style.opacity = 0;
+    imgObserver.observe(image);
+});
